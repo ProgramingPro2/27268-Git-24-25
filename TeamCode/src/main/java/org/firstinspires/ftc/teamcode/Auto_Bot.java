ce5dc9c54b7071cfe43d6d0ac18aa9b67d3e2bb1
ProgramingPro2 <jacksonpearcev@icloud.com>
2024-10-25 16:06:45 -0400

    Add Auto_Bot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Bot.java
@@ -1,13 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Auto_Bot")
-public class Auto_Bot {
+public class Auto_Bot extends LinearOpMode {
     private DcMotor FR;
     private DcMotor FL;
 
+    public void runOpMode() {
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        FL = hardwareMap.get(DcMotor.class, "FL");
 
+        waitForStart();
+
+        while(opModeIsActive()) {
+            // Turn left for 1 second
+            FR.setPower(-1);
+            FL.setPower(1);
+            sleep(1000);
+            // Move forward for 2 seconds
+            FR.setPower(1);
+            FL.setPower(1);
+            sleep(2000);
+            // Turn right for 1 second
+            FR.setPower(1);
+            FL.setPower(-1);
+            sleep(1000);
+        }
+    }
 }
